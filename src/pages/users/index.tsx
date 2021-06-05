54e8c77ac13630ed561777a805d0861e89dabb0a
abauruel <abauruel@gmail.com>
2021-06-05 17:45:04 -0300

    refactor: add axios from fecth api

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,13 +19,13 @@ import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
 import { useQuery } from 'react-query'
+import api from '../../services/api'
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+    const response = await api.get('users')
 
-    const users = data.users.map(user => {
+    const users = response.data.users.map(user => {
       return {
         id: user.id,
         name: user.name,
